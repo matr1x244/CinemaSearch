5583e5325d067a370176497840503005d2dcdbcf
Илья <68413503+matr1x244@users.noreply.github.com>
2021-02-17 18:46:11 +0300

    Delete app/src/main/java/com/geekbrains/kino/ui/home/interactors directory

--- app/src/main/java/com/geekbrains/kino/ui/home/interactors/ImageInteractor.kt
+++
@@ -1,6 +0,0 @@
-package com.geekbrains.kino.ui.home.interactors
-
-interface ImageInteractor {
-    val rusImage: Int?
-    val worldImage: Int?
-}
--- app/src/main/java/com/geekbrains/kino/ui/home/interactors/ImageInteractorImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.geekbrains.kino.ui.home.interactors
-
-import com.geekbrains.kino.R
-
-class ImageInteractorImpl : ImageInteractor {
-    override val rusImage = R.drawable.ic_russia
-    override val worldImage = R.drawable.ic_earth
-}
